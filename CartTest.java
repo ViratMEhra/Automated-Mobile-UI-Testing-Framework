2d7c493e5326ccc90fbc4d91834dda320be818ca
Virat Mehra <119967342+ViratMEhra@users.noreply.github.com>
2025-11-04 19:27:32 +0530

    Create CartTest.java

---
+++ CartTest.java
@@ -0,0 +1,15 @@
+package com.mobile.tests;
+
+import org.testng.annotations.Test;
+
+public class CartTest {
+    @Test
+    public void addToCartTest() {
+        System.out.println("✅ Product added to cart successfully!");
+    }
+
+    @Test
+    public void removeFromCartTest() {
+        System.out.println("✅ Product removed from cart successfully!");
+    }
+}
