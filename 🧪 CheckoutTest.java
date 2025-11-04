92c8ab763d219bfae00040cca8d3dfb42459a11e
Virat Mehra <119967342+ViratMEhra@users.noreply.github.com>
2025-11-04 19:27:59 +0530

    Create 🧪 CheckoutTest.java

---
+++ 🧪 CheckoutTest.java
@@ -0,0 +1,10 @@
+package com.mobile.tests;
+
+import org.testng.annotations.Test;
+
+public class CheckoutTest {
+    @Test
+    public void checkoutTest() {
+        System.out.println("✅ Checkout flow completed successfully!");
+    }
+}
